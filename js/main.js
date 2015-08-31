f36f250cea6f17083518dd706342ef0c6e5b9731
johnny lamb <agnusjack@gmail.com>
2015-08-31 15:47:34 -0600

    fixed forismatic api

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
-var forismatic = require("forismatic-node");
-
-
-
-
-  forismatic.getQuote(function(error, quote) {
-    if (!error) {
-      console.log(quote);
-    } else {
-      console.error(error);
-    }
+$(document).on('ready', function() {
+  $(".btn").click(function(event) {
+    event.preventDefault();
+    getResults();
   });
 
+  function getResults() {
+    $.ajax({
+      url: "http://api.forismatic.com/api/1.0/",
+      jsonp: "jsonp",
+      dataType: "jsonp",
+      data: {
+        method: "getQuote",
+        lang: "en",
+        format: "jsonp"
+      },
+      success: function(apiResponse) {
+        quote = apiResponse.quoteText.trim();
+        author = apiResponse.quoteAuthor;
+        link = apiResponse.quoteLink;
+        $(".results-area").html(quote);
+      }
+    });
+  }
+});
